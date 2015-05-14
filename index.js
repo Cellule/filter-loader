ea4a675a608226ba30c254776688ac2c2e509c18
Michael Ferris <mike.ferris@hotmail.com>
2015-05-14 17:08:59 -0400

    use query param

--- index.js
+++ index.js
@@ -4,14 +4,15 @@ var loaderUtils = require("loader-utils");
 var pick = require("lodash.pick");
 
 module.exports = function(content) {
+  this.cacheable();
   var query = loaderUtils.parseQuery(this.query);
   var data = json5.parse(content);
   if(Array.isArray(data)) {
     data = data.map(function(item) {
-      return pick(item, "name");
+      return pick(item, Object.keys(query));
     });
   } else {
-    data = pick(data, "name");
+    data = pick(data, Object.keys(query));
   }
   return JSON.stringify(data);
 };
